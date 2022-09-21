56003c9a714119ced32d70c4123b019e523c47b5
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-09-20 22:45:55 -0300

    deposit and withdraw tests passing :tada:

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -34,9 +34,36 @@ describe('BankingComponent', () => {
     expect(component.getWallet).toBe(61.3)
   })
 
-  it('(U) setDeposit: should transfer 10.5 to savings', () => {
+  it('(U) setDeposit: should transfer 20.5 to savings', () => {
     component.setDeposit('20.5')
     expect(component.getSavings.toFixed(2)).toBe((31.3).toFixed(2))
     expect(component.getWallet.toFixed(2)).toBe((30.3).toFixed(2))
+  })
+
+  it('(U) setWithdraw: should not to transfer with wrong data', () => {
+    expect(component.setWithdraw('string')).not.toBeTruthy()
+    expect(component.setWithdraw('100')).not.toBeTruthy()
+    expect(component.setDeposit('string')).not.toBeTruthy()
+    expect(component.setDeposit('100')).not.toBeTruthy()
+    expect(component.getSavings).toBe(10.8)
+    expect(component.getWallet).toBe(50.8)
+  })
+
+  it('(I) setDeposit: should transfer 10.5 to savings', () => {
+    let element = fixture.debugElement.nativeElement
+    element.querySelector('#input-deposit').value = '20.5'
+    element.querySelector('#deposit-button').click()
+    fixture.detectChanges()
+    expect(component.getSavings.toFixed(2)).toBe('31.30')
+    expect(element.querySelector('#poup-label').textContent).toEqual('31.30')
+  })
+
+  it('(I) setWithdraw: should transfer 10.5 to wallet', () => {
+    let element = fixture.debugElement.nativeElement
+    element.querySelector('#input-withdraw').value = '10.5'
+    element.querySelector('#withdraw-button').click()
+    fixture.detectChanges()
+    expect(component.getWallet).toBe(61.3)
+    expect(element.querySelector('#wallet-label').textContent).toEqual('61.30')
   })
 })
